625ee5407f0bf5e6c59eea61f77821052f643274
Deven Patel <deven.h.j.patel+softwaredev@gmail.com>
2023-07-17 09:58:24 -0400

    loop challenges

---
+++ loopChallenges/odd1through20.js
@@ -0,0 +1,10 @@
+// Using a loop write code that will console.log all of the odd values from 1 up to 20.
+
+
+    for (var i=0; i<20; i++){
+        if (i % 2 !== 0){
+            console.log(i);
+        }
+    }
+
+
